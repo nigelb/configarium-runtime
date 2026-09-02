058c7aa3b839fa344e257f5a85fd6a17c36d7007
NigelB <nigel.blair@gmail.com>
2026-09-03 06:45:11 +1000

    Add python_logging basic test

---
+++ tests/test_python_logging.py
@@ -0,0 +1,24 @@
+# Configarium-runtime provides runtime utilities for configarium-models.
+# Copyright (C) 2026 NigelB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the configarium-runtime library's python_logging runtime."""
+from configarium_models.models.python_logging import LoggingConfig
+from configarium_runtime.runtime.python_logging import PythonLoggingRuntime
+
+
+def test_basic_config():
+    """Tests for the configarium-runtime library's python_logging runtime."""
+    config = LoggingConfig()
+    PythonLoggingRuntime.apply_model(config)
